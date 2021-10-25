ef21ee93c715e5c1b527622e72b3fe0a5fd9be54
Joao Esperancinha <jofisaes@gmail.com>
2021-10-25 10:00:01 +0200

    🍃 Spring update

---
+++ advanced-library-mngmt-reactive/src/main/kotlin/org/jesperancinha/management/AdvancedReactiveLibraryService.kt
@@ -0,0 +1,15 @@
+package org.jesperancinha.management
+
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+
+
+@SpringBootApplication
+open class AdvancedReactiveLibraryService {
+    companion object {
+        @JvmStatic
+        fun main(args: Array<String>) {
+            SpringApplication.run(AdvancedReactiveLibraryService::class.java, *args)
+        }
+    }
+}
