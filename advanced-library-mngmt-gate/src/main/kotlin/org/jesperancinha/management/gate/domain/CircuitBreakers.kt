7c911edfb3459e686748bcf6254a964402c967c1
Joao Esperancinha <jofisaes@gmail.com>
2022-12-02 12:55:11 +0100

    Updates Dao

--- advanced-library-mngmt-gate/src/main/kotlin/org/jesperancinha/management/gate/domain/CircuitBreakers.kt
+++
@@ -1,8 +0,0 @@
-package org.jesperancinha.management.gate.domain
-
-data class CircuitBreakers(
-    val status: String?,
-    val details: Map<String, Map<String, Any>>?,
-) {
-    constructor() : this(null, null)
-}
