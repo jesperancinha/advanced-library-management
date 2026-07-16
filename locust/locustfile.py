7c44e8ba7226ec2fcf7727f9f90bfeb494efa2e8
jesperancinha <jofisaes@gmail.com>
2026-07-16 13:14:15 +0200

    Codana code improvements

--- locust/locustfile.py
+++ locust/locustfile.py
@@ -2,21 +2,21 @@ from locust import HttpUser, task
 
 class AdvancedLibraryManagementTest(HttpUser):
     @task
-    def g1_GET(self):
+    def g1_get(self):
         self.client.get("http://localhost:8080/api/almg/books/g1/1")
 
     @task
-    def g2_GET(self):
+    def g2_get(self):
         self.client.get("http://localhost:8080/api/almg/books/g2/1")
 
     @task
-    def g3_GET(self):
+    def g3_get(self):
         self.client.get("http://localhost:8080/api/almg/books/g3/1")
 
     @task
-    def g4_GET(self):
+    def g4_get(self):
         self.client.get("http://localhost:8080/api/almg/books/g4/1")
 
     @task
-    def g5_GET(self):
+    def g5_get(self):
         self.client.get("http://localhost:8080/api/almg/books/g5/1")
